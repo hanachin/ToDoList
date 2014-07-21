c993eedbc5db8a434413e72e19ac0baafee397a6
Seiei Higa <hanachin@gmail.com>
2014-07-21 12:34:24 +0900

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Seiei Higa on 2014/07/21.
+//  Copyright (c) 2014年 Seiei Higa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
